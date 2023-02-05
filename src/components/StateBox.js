af713c35ed505faa1d372d01c2c56adb5d23d2b4
Thedinindu Kamburugamuwa <TKamburugamuwa@kaleris.com>
2023-02-05 16:32:16 +0530

    state box error fixed

--- src/components/StateBox.js
+++ src/components/StateBox.js
@@ -1,17 +1,11 @@
 import '../styles/Styles.css'
 import React from 'react'
-import Box from './Box.js'
 
 function StateBox () {
-    const [square, setSquare] = React.useState(Box)
-
-    const elements = square.map(e => {
-        return <div className='box'></div>
-    })
-
+    
     return  (
         <>
-            {elements}
+            <h2>Hi How are you</h2>
         </>
     )
 }
